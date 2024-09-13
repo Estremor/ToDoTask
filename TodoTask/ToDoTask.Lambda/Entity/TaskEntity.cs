a6eb0355ce2a7f249ac315e9e3d3233d6ca1e90e
Alexextremor <alexextremor@gmail.com>
2024-09-13 16:44:49 -0500

    Create and structurate the project

---
+++ TodoTask/ToDoTask.Lambda/Entity/TaskEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace ToDoTask.Lambda.Entity
+{
+    public class TaskEntity
+    {
+        public string TaskId { get; set; }
+        public string TaskName { get; set; }
+        public string TaskDescription { get; set; }
+        public bool IsDone { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
