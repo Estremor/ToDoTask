22d07a1b9d63d9d1f0f2d20ea32c942ed61c324c
Alexextremor <alexextremor@gmail.com>
2024-09-14 11:17:11 -0500

    Add delete function in project

---
+++ TodoTask/ToDoTask.Lambda/FunctionDelete.cs
@@ -0,0 +1,37 @@
+﻿using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using Microsoft.Extensions.DependencyInjection;
+using ToDoTask.Lambda.Entity;
+using ToDoTask.Lambda.Repository;
+
+namespace ToDoTask.Lambda;
+public class FunctionDelete
+{
+    #region property
+
+    private readonly IRepository<TaskEntity> _repository;
+
+    #endregion
+
+    #region C'tor
+
+    public FunctionDelete()
+    {
+        _repository = Configuration
+            .ConfigureServices()
+            .GetRequiredService<IRepository<TaskEntity>>();
+    }
+
+    #endregion
+
+    public async Task<APIGatewayProxyResponse> DeleteTaskHandler(string taskId, ILambdaContext context)
+    {
+
+        await _repository.DeleteAsync(new TaskEntity { Id = taskId });
+
+        return new APIGatewayProxyResponse
+        {
+            StatusCode = 204
+        };
+    }
+}
